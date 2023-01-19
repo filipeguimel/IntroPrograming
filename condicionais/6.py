6590916fba6a9861936392549f5340a03479e0cd
filipeguimel <guimelfilipe@hotmail.com>
2023-01-18 23:49:25 -0300

    add program

---
+++ condicionais/6.py
@@ -0,0 +1,24 @@
+# Input
+
+# A entrada é composta por 1 linha. A qual será a frase dita pelo Narrador
+# S
+# S é uma string
+
+# Output
+
+# A saída consiste em 1 linha a qual responderá a Sanji o que ele deverá fazer.
+
+s=str(input())
+
+if s=='Olha a chuva!':
+  print('Grite choveu!')
+elif s=='Olha a cobra!':
+  print('Grite é mentira')
+elif s=='Cavalheiros ajoelhados de frente pras damas!':
+  print('Ajoelhe-se na frente da dama, beije a mão dela e grite beijou!')
+elif s=='Alavantu!':
+  print('Vá em direção a sua dama')
+elif s=='Anarriê!':
+  print('Se distancie da sua dama')
+elif s=='Balancê!':
+  print('Dance com a sua dama')
